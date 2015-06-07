d10791b69b7b90d5d00d7afe63485db01ae015a8
Dory <Dory@Dory-PC>
2015-06-07 18:57:01 +0300

    there still just the criteria of the months text field ...

---
+++ public_html/Script.js
@@ -0,0 +1,54 @@
+function validateAmount(e) {
+    var key = e.keyCode;
+    if ((key >= 48 && key <= 57) || (key >= 96 && key <= 105) || (key === 8)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validateRate(e) {
+    var key = e.keyCode;
+    var value = document.getElementById("rateTxt").value;
+    if ((key >= 48 && key <= 57) || (key >= 96 && key <= 105) || (key === 8)) {
+        if ((value === null) || (value.trim() === "")) {
+            return true;
+        }
+        else if ((key === 48) || (key === 96) || (key === 8)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+function validateMonths(e) {
+    var key = e.keyCode;
+    var value = document.getElementById("monthsTxt").value;
+    if ((key >= 48 && key <= 57) || (key >= 96 && key <= 105) || (key === 8)) {
+        if ((value === null) || (value.trim() === "")) {
+            return true;
+        }
+        else if ((key === 48) || (key === 96) || (key === 49) || (key === 97) || (key === 50) || (key === 98) || (key === 8)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+function calculate() {
+    var amount = document.getElementById("amountTxt").value;
+    var rate = document.getElementById("rateTxt").value;
+    var months = document.getElementById("monthsTxt").value;
+    document.getElementById("interest").value = amount * rate * months / 100;
+}
